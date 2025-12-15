5764c969f53ad25ef47927d9990c013b40fa62e5
sanyath007 <sanyath007@gmail.com>
2025-12-15 14:35:01 +0700

    reflactor: completed functionalities of both AuthProvider and Login components

--- src/contexts/AuthContect.tsx
+++ src/contexts/AuthContect.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import api from "../api";
 
 interface AuthContextType {
@@ -15,19 +15,44 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<any>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const login = async ({ username, password }): Promise<{ success: boolean, message: string }> => {
-        try {
-            const res = await api.post(`/api/auth/`, { username, password });
-            console.log(res);
+    useEffect(() => {
+        const initializeAuth = async () => {
+            const token = localStorage.getItem('access_token');
+            const storedUser = localStorage.getItem('auth_user');
 
+            if (token && storedUser) {
+                setUser(storedUser);
+            } else {
+                localStorage.removeItem('access_token');
+                localStorage.removeItem('auth_user');
+            }
+        };
+
+        initializeAuth();
+    }, []);
+
+    const login = async ({ email, password }): Promise<{ success: boolean, message: string }> => {
+        try {
+            setIsLoading(true);
+
+            const res = await api.post(`/api/auth/login`, { email, password });
+
+            localStorage.setItem('access_token', res.data.access_token);
+            localStorage.setItem('auth_user', res.data.user);
+            setUser(res.data.user);
             return { success: true, message: ''};
         } catch (error) {
             return { success: false, message: error.message };
+        } finally {
+            setIsLoading(false);
         }
     };
 
     const logout = async (): Promise<void> => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('auth_user');
 
+        setUser(null);
     };
 
     const value: AuthContextType = {
--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [credentials, setCredentials] = useState<any>({ email: '', password: '' });
+    const [errors, setErrors] = useState<any>(null)
+    const { login, isAuthenticated } = useAuth();
+
+    const handleLogin = async (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const result = await login(credentials);
+
+            if (result.success) {
+                navigate('/');
+            }
+        } catch (error) {
+            setErrors(error)
+        }
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCredentials(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    if (isAuthenticated) {
+        navigate('/');
+    }
+
     return (
         <div className="border px-4 py-6 rounded-lg shadow-lg bg-white w-3/12">
             <div className="flex flex-col space-y-4">
@@ -8,13 +40,15 @@ const Login = () => {
                     <h1 className="text-2xl font-bold">Login Page</h1>
                 </div>
 
-                <form className="space-y-4">
+                <form onSubmit={handleLogin} className="space-y-4">
                     <div className="flex flex-col">
                         <label htmlFor="email">Email:</label>
                         <input
                             type="email"
                             name="email"
-                            className="border border-indigo-400 rounded-lg p-1"
+                            value={credentials.email}
+                            onChange={handleChange}
+                            className="border border-indigo-400 rounded-lg py-1 px-3"
                         />
                     </div>
                     <div className="flex flex-col">
@@ -22,10 +56,15 @@ const Login = () => {
                         <input
                             type="password"
                             name="password"
-                            className="border border-indigo-400 rounded-lg p-1"
+                            value={credentials.password}
+                            onChange={handleChange}
+                            className="border border-indigo-400 rounded-lg py-1 px-3"
                         />
                     </div>
-                    <button className="w-full border bg-indigo-400 p-2 text-white rounded-lg hover:bg-indigo-500">
+                    <button
+                        type="submit"
+                        className="w-full border bg-indigo-400 p-2 text-white rounded-lg hover:bg-indigo-500"
+                    >
                         Log in
                     </button>
                 </form>
