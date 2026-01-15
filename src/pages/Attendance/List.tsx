a09a45eb96d66bc6c91c4095d1b7f330566494d9
sanyath007 <sanyath007@gmail.com>
2026-01-15 16:59:58 +0700

    reflactor: add fetching attendances data logic with related local states

--- src/pages/Attendance/List.tsx
+++ src/pages/Attendance/List.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CalendarClock } from 'lucide-react';
+import moment from 'moment';
+import api from '../../api';
 
 const AttendanceList = () => {
+    const [attendances, setAttendances] = React.useState([]);
+    const [currentDate, setCurrentDate] = React.useState(moment().format('YYYY-MM-DD'));
+
+    useEffect(() => {
+        getAttendances(currentDate)
+    }, []);
+
+    const getAttendances = async (date: string) => {
+        const response = await api.get(`/api/attendances/check-time/${date}/daily`);
+
+        setAttendances(response.data.daily);
+    }
+
     return (
         <div className="max-w-4xl mx-auto">
             {/* Header */}
@@ -38,7 +53,33 @@ const AttendanceList = () => {
             
             {/* Main Content */}
             <div className="bg-white rounded-lg shadow-lg p-6">
-                {/*  */}
+                {attendances.filter((attendance: any) => attendance.CheTmType === 'เข้า').map((attendance: any) => (
+                    <div key={attendance.id} className="flex flex-row items-center gap-4 border-b last:border-0 py-4">
+                        {attendance.CheTmPic ? (
+                        <img
+                            src={`https://mhc9dmh.com/DATA/PhotoCheckTime/${attendance.CheTmPic}`}
+                            className='w-16 h-16 object-cover rounded-md'
+                            alt='check-in-pic'
+                        />
+                        ) : (
+                            <img
+                            src={`https://ui-avatars.com/api/?name=John+Doe&background=${attendance.employee?.EmColor}&color=fff&size=128`}
+                            className='w-16 h-16 object-cover rounded-md'
+                            alt='check-in-pic'
+                        />
+                        )}
+
+                        <div>
+                            <h2 className="text-lg font-semibold text-blue-800">
+                                {attendance.employee ? attendance.employee?.EmName : 'Unknown Employee'}
+                            </h2>
+                            <p className="text-gray-600">
+                                วันที่: {moment(attendance.CheTmDate).format('DD/MM/YYYY')} <br />
+                                เวลา: {moment(attendance.CheTmDate).format('HH:mm:ss')}
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
